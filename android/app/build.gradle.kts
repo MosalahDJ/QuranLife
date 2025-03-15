75b98b57ec1ba0ccbbea449653805a91dd5e1195
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-03-16 00:19:05 +0100

    store secret key and use it

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,20 +21,17 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.MoSalahDJ.quran_life"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        
+        manifestPlaceholders["mapsApiKey"] = project.properties["mapsApiKey"] as? String ?: ""
     }
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
