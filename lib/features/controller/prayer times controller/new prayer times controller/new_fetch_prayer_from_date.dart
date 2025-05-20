59cc0a0487f532a6e60495733139e587e6165457
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-05-20 12:25:45 +0100

    refactor(prayer times controller): improve debug logging and data access in fetchPrayerTimes
    Enhance debug logging to include more detailed information about the month's data structure. Simplify the access to timings by first extracting the month's data array, improving readability and maintainability of the code.

--- lib/features/controller/prayer times controller/new prayer times controller/new_fetch_prayer_from_date.dart
+++ lib/features/controller/prayer times controller/new prayer times controller/new_fetch_prayer_from_date.dart
@@ -82,15 +82,20 @@ class NewFetchPrayerFromDate extends GetxController {
     try {
       prayersdayskeys = prayerData.keys.toList();
       for (int i = 0; i < prayersdayskeys.length; i++) {
-        // Access the first element of the array to get timings
-        var timings = prayerData[prayersdayskeys[i]][0]['timings'];
+        // Get the month's data array
+        var monthData = prayerData[prayersdayskeys[i]];
         
         if (i == 0) {
           print("________________________________________________________");
-          print("Debug - timings: $timings");
-          print("Debug - Fajr time: ${timings['Fajr']}");
+          print("Month key: ${prayersdayskeys[i]}");
+          print("Month data: $monthData");
+          print("First day timings: ${monthData[0]['timings']}");
+          print("Fajr time: ${monthData[0]['timings']['Fajr']}");
           print("________________________________________________________");
         }
+        
+        // Get timings from the first day of the month
+        var timings = monthData[0]['timings'];
         
         Map<String, String> dailyPrayers = {
           'Fajr': timings['Fajr'],
@@ -105,7 +110,7 @@ class NewFetchPrayerFromDate extends GetxController {
       }
     } catch (e) {
       print('Error in fetchPrayerTimes: $e');
-      print('Current prayerData structure: $prayerData');
+      print('Prayer Data structure: $prayerData');
     }
   }
 }
