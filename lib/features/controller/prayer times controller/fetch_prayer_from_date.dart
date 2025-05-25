7fadcb3b8695a54a4a6dd672af13fea8ca87e4b0
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-05-25 15:05:15 +0100

    docs(fetch_prayer_from_date): update comment for getDateByIndex function
    Clarify the purpose of the getDateByIndex function by adding details about checking the current date and displaying the current salat time with a different color

--- lib/features/controller/prayer times controller/fetch_prayer_from_date.dart
+++ lib/features/controller/prayer times controller/fetch_prayer_from_date.dart
@@ -72,10 +72,10 @@ class FetchPrayerFromDate extends GetxController {
       <String, Map<String, Map<String, String>>>{}.obs;
   DateTime currentDate = DateTime.now();
 
-  // Function to get the date for a given index
+  // Function to get the date for a given index for check if we are in current
+  // date to to display the current slat time with a differnt color
   String? getDateByIndex(int index) {
     if (firstResponseDate == null) return null;
-
     // Calculate the date for the given index
     final date = firstResponseDate!.add(Duration(days: index));
 
