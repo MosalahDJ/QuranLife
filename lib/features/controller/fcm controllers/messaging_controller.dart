034ada781670daafd732017f65d5eb56502c6841
mohamedsalah <djehelmohamedsalah@gmail.com>
2025-05-26 14:23:33 +0100

    refactor(auth): move user validation logic to messaging controller
    The user existence and ban check logic was duplicated in both MessagingController and SplashViewController. This commit:
    1. Moves the _checkIfUserExists() method to MessagingController where it's more appropriate
    2. Updates SplashViewController to rely on MessagingController for user validation
    3. Improves code organization and reduces duplication

--- lib/features/controller/fcm controllers/messaging_controller.dart
+++ lib/features/controller/fcm controllers/messaging_controller.dart
@@ -3,14 +3,89 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:project/features/controller/fcm%20controllers/fcm_controller.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MessagingController extends GetxController {
   final TextEditingController messageController = TextEditingController();
   final ScrollController scrollController = ScrollController();
   final FCMController fcmController = Get.find();
   final currentUser = FirebaseAuth.instance.currentUser;
+  @override
+  void onInit() async {
+    await _checkIfUserExists();
+    super.onInit();
+  }
+
+  Future<bool> _checkIfUserExists() async {
+    try {
+      final User? currentUser = FirebaseAuth.instance.currentUser;
+      if (currentUser == null) return false;
+
+      final userDoc =
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(currentUser.uid)
+              .get();
+
+      if (!userDoc.exists) {
+        await FirebaseAuth.instance.signOut();
+        return false;
+      }
+
+      if (userDoc.data()?['isBanned'] == true) {
+        await FirebaseAuth.instance.signOut();
+
+        await Get.dialog(
+          AlertDialog(
+            title: Text('account_banned'.tr),
+            content: Text('violation_rules'.tr),
+            actions: [
+              Column(
+                children: [
+                  TextButton(
+                    onPressed: () async {
+                      // Replace with your support account URL
+                      final Uri url = Uri.parse('https://t.me/0655663020');
+                      if (await canLaunchUrl(url)) {
+                        await launchUrl(url);
+                      }
+                    },
+                    child: Text(
+                      'contact_support_if_error'.tr,
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      SystemNavigator.pop(); // Close the app
+                    },
+                    child: Text('ok'.tr),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          barrierDismissible: false, // User must use one of the buttons
+        );
+
+        return false;
+      }
+
+      await currentUser.reload();
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        await FirebaseAuth.instance.signOut();
+      }
+      return false;
+    } catch (e) {
+      return false;
+    }
+  }
 
   // Show a dialog using AwesomeDialog
   void _showDialog(
@@ -41,7 +116,7 @@ class MessagingController extends GetxController {
         await Connectivity().checkConnectivity();
     if (conectivity.contains(ConnectivityResult.none)) {
       _showDialog(
-      // ignore: use_build_context_synchronously
+        // ignore: use_build_context_synchronously
         context,
         'no_internet'.tr,
         'internet_required_for_sendmessage'.tr,
--- lib/features/controller/spalshview controller/splash_view_controller.dart
+++ lib/features/controller/spalshview controller/splash_view_controller.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:project/features/controller/Auth%20controller/user_state_controller.dart';
 import 'package:project/features/controller/prayer%20times%20controller/fetch_prayer_from_date.dart';
@@ -10,7 +8,6 @@ import 'package:project/features/controller/prayer%20times%20controller/get_resp
 import 'package:project/features/controller/prayer%20times%20controller/location_controller.dart';
 import 'package:project/features/controller/prayer%20times%20controller/deterimine_prayers_controller.dart';
 import 'package:project/features/controller/prayer%20times%20controller/times_page_controller.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class SplashViewController extends GetxController
     with GetSingleTickerProviderStateMixin {
@@ -29,7 +26,6 @@ class SplashViewController extends GetxController
   RxBool isLoading = true.obs;
   void tonextpage() {
     Future.delayed(const Duration(seconds: 2), () async {
-      final bool userExists = await _checkIfUserExists();
       final User? currentUser = FirebaseAuth.instance.currentUser;
       final connectivityResult = await Connectivity().checkConnectivity();
       if (connectivityResult.contains(ConnectivityResult.none)) {
@@ -46,7 +42,7 @@ class SplashViewController extends GetxController
             break;
         }
       } else {
-        if (!userExists || currentUser == null) {
+        if (currentUser == null) {
           // User is not logged in
           Get.offAllNamed("onboarding");
         } else if (currentUser.isAnonymous) {
@@ -63,6 +59,19 @@ class SplashViewController extends GetxController
     });
   }
 
+  void getdata() async {
+    bool wasDataRefreshed = await responsectrl.initileresponse();
+    isLoading(true);
+    // Only fetch prayer times if we didn't just refresh the data
+    if (!wasDataRefreshed) {
+      await fpfctrl.loadPrayerData();
+    }
+    prayerctrl.determineCurrentPrayer();
+    timespagectrl.pagecontroller();
+    timespagectrl.getcurrentpage();
+    isLoading(false);
+  }
+
   @override
   void onInit() async {
     super.onInit();
@@ -75,90 +84,12 @@ class SplashViewController extends GetxController
       end: 1,
     ).animate(animationcontroller!);
     animationcontroller?.repeat(reverse: true);
-    isLoading(true);
-    bool wasDataRefreshed = await responsectrl.initileresponse();
-    // Only fetch prayer times if we didn't just refresh the data
-    if (!wasDataRefreshed) {
-      await fpfctrl.loadPrayerData();
-    }
-    prayerctrl.determineCurrentPrayer();
-    timespagectrl.pagecontroller();
-    timespagectrl.getcurrentpage();
-    isLoading(false);
+    getdata();
     tonextpage();
 
     // Start periodic check after initial setup
     Get.find<GetResponseBody>().startPeriodicCheck();
   }
-
-  Future<bool> _checkIfUserExists() async {
-    try {
-      final User? currentUser = FirebaseAuth.instance.currentUser;
-      if (currentUser == null) return false;
-
-      final userDoc =
-          await FirebaseFirestore.instance
-              .collection('users')
-              .doc(currentUser.uid)
-              .get();
-
-      if (!userDoc.exists) {
-        await FirebaseAuth.instance.signOut();
-        return false;
-      }
-
-      if (userDoc.data()?['isBanned'] == true) {
-        await FirebaseAuth.instance.signOut();
-
-        await Get.dialog(
-          AlertDialog(
-            title: Text('account_banned'.tr),
-            content: Text('violation_rules'.tr),
-            actions: [
-              Column(
-                children: [
-                  TextButton(
-                    onPressed: () async {
-                      // Replace with your support account URL
-                      final Uri url = Uri.parse('https://t.me/0655663020');
-                      if (await canLaunchUrl(url)) {
-                        await launchUrl(url);
-                      }
-                    },
-                    child: Text(
-                      'contact_support_if_error'.tr,
-                      textAlign: TextAlign.center,
-                      style: const TextStyle(color: Colors.blue),
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      SystemNavigator.pop(); // Close the app
-                    },
-                    child: Text('ok'.tr),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          barrierDismissible: false, // User must use one of the buttons
-        );
-
-        return false;
-      }
-
-      await currentUser.reload();
-      return true;
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        await FirebaseAuth.instance.signOut();
-      }
-      return false;
-    } catch (e) {
-      return false;
-    }
-  }
-
   @override
   void onClose() {
     super.onClose();
