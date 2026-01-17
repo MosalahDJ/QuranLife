2d810c9e5beb841be9b8c55fa03dfb5f1b0fa459
mohamedsalah <djehelmohamedsalah@gmail.com>
2026-01-17 16:21:35 +0100

    remove unused Kotlin compiler session file and update build.gradle.kts for Android library configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,6 +11,12 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
+    
+    plugins.withId("com.android.library") {
+        configure<com.android.build.gradle.LibraryExtension> {
+            compileSdk = 36
+        }
+    }
 }
 subprojects {
     project.evaluationDependsOn(":app")
